d141884c1183e90cc2683fb810055e1c6260e667
Rabiawaheed786 <162490318+Rabiawaheed786@users.noreply.github.com>
2024-10-31 02:26:04 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import Hero from './components/hero';
+import Features from './components/features';
+
+
+export default function  Home() {
+  return (
+    <div> 
+      <Hero />
+      <Features />
+    </div>
+  )
+}
